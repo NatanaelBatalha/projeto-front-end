5653e012ba1ab73a606bdb4a5a7655b7a3b529b2
Natanael <natan_batalha@yahoo.com.br>
2024-10-23 20:59:41 -0300

    casa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,47 +2,64 @@ import logoFooter from "../assets/logo-footer.svg"
 import facebook from "../assets/facebook.svg"
 import instagram from "../assets/instagram.svg"
 import twitter from "../assets/twitter.svg"
+import "primeflex/primeflex.css"
+
 
 const Footer = () => {
-    return ( 
+    return (
         <>
             <div id="rodape">
-                <img src={logoFooter} alt="Logo Rodapé" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maxime optio natus ratione iure alias eaque enim temporibus, fugiat laborum, sapiente magni neque pariatur sunt accusantium. Rerum placeat illo debitis.</p>
-                <div id="redes">
-                    <img src={facebook} alt="Facebook" />
-                    <img src={instagram} alt="Instagram" />
-                    <img src={twitter} alt="Twitter" />
+                <div className="flex text-justify" >
+                    <div className="flex-1  p-4 m-3 mr-7">
+                        <div className="flex m-3">
+                            <img
+                                src={logoFooter}
+                                alt="Logo Rodapé"
+                            />
+                        </div>
+                        <div className="flex m-3 text-left">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maxime optio natus ratione iure alias eaque enim temporibus, fugiat laborum, sapiente magni neque pariatur sunt accusantium. Rerum placeat illo debitis.</p>
+                        </div>
+                        <div className="flex m-3" id="redes">
+                            <img src={facebook} alt="Facebook" className="mr-5" />
+                            <img src={instagram} alt="Instagram" className="mr-5" />
+                            <img src={twitter} alt="Twitter" className="mr-5"/>
+                        </div>
+                    </div>
+                    <div className="flex-1 text-left p-4 m-3 mr-7" id="informacao">
+                        <h2>Informação</h2>
+                        <p>Sobre Drip Store</p>
+                        <p>Segurança</p>
+                        <p>Wishlist</p>
+                        <p>Blog</p>
+                        <p>Trabalhe conosco</p>
+                        <p>Meus Pedidos</p>
+                    </div>
+                    <div className="flex-1 text-left p-4 m-3" id="categoria">
+                        <h2>Categorias</h2>
+                        <p>Camisetas</p>
+                        <p>Calças</p>
+                        <p>Bonés</p>
+                        <p>Headphones</p>
+                        <p>Tênis</p>
+                    </div>
+                    <div className="flex-1 text-left p-4 m-3" id="contato">
+                        <h2>Contato</h2>
+                        <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+                        <p>85 3051-3411</p>
+                    </div>
                 </div>
-                <div id="informacao">
-                    <h3>Imformação</h3>
-                    <p>Sobre Drip Store</p>
-                    <p>Segurança</p>
-                    <p>Wishlist</p>
-                    <p>Blog</p>
-                    <p>Trabalhe conosco</p>
-                    <p>Meus Pedidos</p>
-                </div>
-                <div id="categoria">
-                    <h3>Categorias</h3>
-                    <p>Camisetas</p>
-                    <p>Calças</p>
-                    <p>Bonés</p>
-                    <p>Headphones</p>
-                    <p>Tênis</p>
-                </div>
-                <div id="contato">
-                    <h3>Contato</h3>
-                    <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
-                    <p>85 3051-3411</p>
-                </div>
-                <hr />
+                
                 <div id="digital">
-                    <p>@ 2022 Digital College</p>
+                    <hr />
+                    <div>
+                        <p>@ 2022 Digital College</p>
+                    </div>
                 </div>
             </div>
+            
         </>
-     );
+    );
 }
- 
+
 export default Footer;
